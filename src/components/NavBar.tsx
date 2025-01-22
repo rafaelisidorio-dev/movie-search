7cf34eb16c247644580a912f51367151f931ecf8
henriquerocharafael <rafaelrocha2500@gmail.com>
2025-01-22 05:58:24 -0300

    refactor: rename variables

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,31 +2,26 @@ import { ChangeEvent } from "react";
 import Logo from "/logo.svg";
 
 interface NavBarProps {
-  movieSearch: string;
-  setMovieSearch: React.Dispatch<React.SetStateAction<string>>;
-  handleSubmit: (event: ChangeEvent<HTMLFormElement>) => void;
+  searchTerm: string;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export function NavBar({
-  movieSearch,
-  setMovieSearch,
-  handleSubmit,
-}: NavBarProps) {
+export function NavBar({ searchTerm, setSearchTerm }: NavBarProps) {
   return (
     <header>
       <nav className="bg-sky-950 py-5">
         <div className="max-w-7xl flex items-center justify-between my-0 mx-auto">
           <img src={Logo} className="w-37 h-5" />
 
-          <form onSubmit={handleSubmit}>
+          <form>
             <input
               className="mr-2 text-black rounded-md outline-none"
               type="text"
               placeholder="Search Movie"
               onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                setMovieSearch(event.target.value)
+                setSearchTerm(event.target.value)
               }
-              value={movieSearch}
+              value={searchTerm}
             />
             <button className="text-white border rounded-md" type="submit">
               Search
